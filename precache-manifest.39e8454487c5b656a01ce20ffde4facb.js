19cb7eeb729fe6b3ecdefc40bfe358c0ee69f79b
Lucas Harvey <l4harvey@uwaterloo.ca>
2019-01-20 16:49:43 -0500

    Updates

---
+++ precache-manifest.39e8454487c5b656a01ce20ffde4facb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd735f366eb2d9096e2b1478cb3550c6",
+    "url": "/static/media/me.cd735f36.png"
+  },
+  {
+    "revision": "d907449ba6ccd463bf234997549c6c44",
+    "url": "/static/media/intellisports.d907449b.png"
+  },
+  {
+    "revision": "3995246cad358a3a249dd0f6f4c6a83c",
+    "url": "/static/media/graph-gen.3995246c.png"
+  },
+  {
+    "revision": "b3d2d8c046d1ae3cb56f13a39e8bd136",
+    "url": "/static/media/LucasHarveyResume.b3d2d8c0.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "008d9f90fa36e94cdefe",
+    "url": "/static/js/main.008d9f90.chunk.js"
+  },
+  {
+    "revision": "e60da1c48ac13a35fb26",
+    "url": "/static/js/1.e60da1c4.chunk.js"
+  },
+  {
+    "revision": "008d9f90fa36e94cdefe",
+    "url": "/static/css/main.c63dd051.chunk.css"
+  },
+  {
+    "revision": "6f623177e45f15f2ae0cb5ea2202bce7",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.401404a1eb7fac409dd146399c0e9576.js"
+  "/precache-manifest.39e8454487c5b656a01ce20ffde4facb.js"
 );
 
 workbox.clientsClaim();
