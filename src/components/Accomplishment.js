41dc8c3afd0fccf07887f06bdc3bb854cfd2c34a
Lucas Harvey <l.harvey@intellisports.net>
2019-08-12 14:58:44 -0400

    Update website

--- src/components/Accomplishment.js
+++ src/components/Accomplishment.js
@@ -10,12 +10,12 @@ class Accomplishment extends Component {
                 <div className="accomplishment">
                     <a className="card" target="_blank" rel="noopener noreferrer" href={link}><img alt={alt} src={image}></img></a>
                     <div className="accomplishment-info">
-                        <p className="accomplishment-title">{title}</p> 
+                        <p className="accomplishment-title">{title}</p>
                         <p className="accomplishment-subtitle1">{subtitle1}</p>
                         <p className="accomplishment-subtitle2">{subtitle2}</p>
                         <p className="accomplishment-description">{description}</p>
                     </div>
-                </div>        
+                </div>
             </ScrollAnimation>
         );
     }
