9cf43c389023cce0fc5a7dd78e36b8b84b73695d
Lucas Harvey <l.harvey@intellisports.net>
2020-04-07 12:32:48 -0400

    remove header image, add navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import resumeIcon from "./Images/resume.png";
 import githubIcon from "./Images/github.png";
 import linkedinIcon from "./Images/linkedin.png";
 import emailIcon from "./Images/email.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import { HashLink as Link } from "react-router-hash-link";
 import Routing from "./components/Routing.js";
 import smoothscroll from "smoothscroll-polyfill";
@@ -14,7 +17,52 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="navbar">
+        <nav className="navbar nav-expand-lg navbar-light custom-navbar">
+          <div className="navbar-brand custom-navbar-brand">Lucas Harvey</div>
+          <Nav className="ml-auto">
+            <Nav.Item className="nav-item">
+              <Link
+                to="/#about-me"
+                scroll={el =>
+                  el.scrollIntoView({ behavior: "smooth", block: "start" })
+                }
+              >
+                ABOUT ME
+              </Link>
+            </Nav.Item>
+            <Nav.Item className="nav-item">
+              <Link
+                to="/#skills"
+                scroll={el =>
+                  el.scrollIntoView({ behavior: "smooth", block: "start" })
+                }
+              >
+                SKILLS
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link
+                to="/#work"
+                scroll={el =>
+                  el.scrollIntoView({ behavior: "smooth", block: "start" })
+                }
+              >
+                WORK
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link
+                to="/#projects"
+                scroll={el =>
+                  el.scrollIntoView({ behavior: "smooth", block: "start" })
+                }
+              >
+                PROJECTS
+              </Link>
+            </Nav.Item>
+          </Nav>
+        </nav>
+        {/* <div className="navbar">
           <Link
             to="/#about-me"
             scroll={el =>
@@ -47,8 +95,8 @@ class App extends Component {
           >
             PROJECTS
           </Link>
-        </div>
-        <header id="header" className="App-header">
+        </div> */}
+        {/* <header id="header" className="App-header">
           <div className="contact">
             <a target="_blank" rel="noopener noreferrer" href={resume}>
               <img alt="resume icon" src={resumeIcon} />
@@ -71,7 +119,7 @@ class App extends Component {
               <img alt="email icon" src={emailIcon} />
             </a>
           </div>
-        </header>
+        </header> */}
         <Routing />
       </div>
     );
