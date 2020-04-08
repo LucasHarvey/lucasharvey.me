de3b4d309eb1cfc0fa69ad9d077bc9f03a059d02
Lucas Harvey <l.harvey@intellisports.net>
2020-04-08 12:17:52 -0400

    Updates

---
+++ precache-manifest.c108dafefe4a7a16348cc8fbc59904f9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d907449ba6ccd463bf234997549c6c44",
+    "url": "/static/media/intellisports.d907449b.png"
+  },
+  {
+    "revision": "61011d4a8d87aafcec3b",
+    "url": "/static/css/main.9b65e07f.chunk.css"
+  },
+  {
+    "revision": "0e1ab8b77ec5cf1558d40c5881375683",
+    "url": "/static/media/round_me.0e1ab8b7.png"
+  },
+  {
+    "revision": "1ee12a064902cf0d1d4a",
+    "url": "/static/js/1.1ee12a06.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b9be09358baf41a1e8fcf1d74ec01d8",
+    "url": "/static/media/LucasHarveyResume.0b9be093.pdf"
+  },
+  {
+    "revision": "61011d4a8d87aafcec3b",
+    "url": "/static/js/main.61011d4a.chunk.js"
+  },
+  {
+    "revision": "191610f4ea24eed6c60e3dd9b9b0c174",
+    "url": "/static/media/partyfinder.191610f4.png"
+  },
+  {
+    "revision": "505eaa36325275aaecbda16f561556cd",
+    "url": "/static/media/wayfair.505eaa36.png"
+  },
+  {
+    "revision": "3995246cad358a3a249dd0f6f4c6a83c",
+    "url": "/static/media/graph-gen.3995246c.png"
+  },
+  {
+    "revision": "1ee12a064902cf0d1d4a",
+    "url": "/static/css/1.9dbabc63.chunk.css"
+  },
+  {
+    "revision": "75a8358683ace38f0d0f1bbcf842f469",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b47aced66c34b6da17e13fddc452e49.js"
+  "/precache-manifest.c108dafefe4a7a16348cc8fbc59904f9.js"
 );
 
 workbox.clientsClaim();
